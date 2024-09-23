810bfea6750a73e9ccd30a32ec7a44d0aca099d2
PRAGYANSHREE DAS <pragyan9423@gmail.com>
2024-09-23 15:32:29 +0530

    initial commit

---
+++ app/src/main/java/com/ocean/mycodelab1/MainActivity.kt
@@ -0,0 +1,87 @@
+package com.ocean.mycodelab1
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.ocean.mycodelab1.ui.theme.MyGoogleCodeLab1Theme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            MyGoogleCodeLab1Theme {
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    ComposeArticleApp()
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun ComposeArticleApp() {
+    ArticleCard(
+        title = stringResource(id = R.string.title_jetpack_compose_tutorial),
+        shortDescription = stringResource(id = R.string.compose_short_desc),
+        longDescription = stringResource(id = R.string.compose_long_desc),
+        imagePainter = painterResource(id = R.drawable.bg_compose_background)
+    )
+}
+
+@Composable
+private fun ArticleCard(
+    title: String,
+    shortDescription: String,
+    longDescription: String,
+    imagePainter: Painter,
+    modifier: Modifier = Modifier,
+) {
+    Column(modifier = modifier) {
+        Image(painter = imagePainter, contentDescription = null)
+        Text(
+            text = title,
+            modifier = Modifier.padding(16.dp),
+            fontSize = 24.sp
+        )
+        Text(
+            text = shortDescription,
+            modifier = Modifier.padding(start = 16.dp, end = 16.dp),
+            textAlign = TextAlign.Justify
+        )
+        Text(
+            text = longDescription,
+            modifier = Modifier.padding(16.dp),
+            textAlign = TextAlign.Justify
+        )
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    MyGoogleCodeLab1Theme {
+        ComposeArticleApp()
+    }
+}
